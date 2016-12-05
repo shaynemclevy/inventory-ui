2ce1629f05730fa08e249a552c9cc0b32c19e429
Shayne McLevy <shaynemclevy@gmail.com>
2016-12-04 23:21:42 -0500

    thing

--- assets/js/inventory.js
+++ assets/js/inventory.js
@@ -1,132 +1,15 @@
 function getData() {
-	return [
-    {
-        "id": "1",
-        "name": "Conference Room #1",
-        "type": "Conference Room",
-        "status": "Available"
-    },
-    {
-        "id": "2",
-        "name": "Projector #1",
-        "type": "Projector",
-        "status": "Available"
-    },
-    {
-        "id": "3",
-        "name": "Whiteboard #1",
-        "type": "Whiteboard",
-        "status": "Unavailable"
-    },
-    {
-        "id": "4",
-        "name": "Computer #1",
-        "type": "Computer",
-        "status": "Unavailable"
-    },
-    {
-        "id": "5",
-        "name": "Conference Room #2",
-        "type": "Conference Room",
-        "status": "Available"
-    },
-	{
-        "id": "6",
-        "name": "Projector #2",
-        "type": "Projector",
-        "status": "Unavailable"
-    },
-    {
-        "id": "7",
-        "name": "Whiteboard #2",
-        "type": "Whiteboard",
-        "status": "Unavailable"
-    },
-    {
-        "id": "8",
-        "name": "Computer #2",
-        "type": "Computer",
-        "status": "Available"
-    },
-    {
-        "id": "9",
-        "name": "Conference Room #3",
-        "type": "Conference Room",
-        "status": "Unavailable"
-    },
-    {
-        "id": "10",
-        "name": "Projector #3",
-        "type": "Projector",
-        "status": "Available"
-    },
-	{
-        "id": "11",
-        "name": "Whiteboard #3",
-        "type": "Whiteboard",
-        "status": "Unavailable"
-    },
-    {
-        "id": "12",
-        "name": "Computer #3",
-        "type": "Computer",
-        "status": "Available"
-    },
-    {
-        "id": "13",
-        "name": "Conference Room #4",
-        "type": "Conference Room",
-        "status": "Unavailable"
-    },
-    {
-        "id": "14",
-        "name": "Projector #4",
-        "type": "Projector",
-        "status": "Unavailable"
-    },
-    {
-        "id": "15",
-        "name": "Whiteboard #4",
-        "type": "Whiteboard",
-        "status": "Available"
-    },
-	{
-        "id": "16",
-        "name": "Computer #4",
-        "type": "Computer",
-        "status": "Unavailable"
-    },
-    {
-        "id": "17",
-        "name": "Conference Room #5",
-        "type": "Conference Room",
-        "status": "Available"
-    },
-    {
-        "id": "18",
-        "name": "Projector #5",
-        "type": "Projector",
-        "status": "Unavailable"
-    },
-    {
-        "id": "19",
-        "name": "Whiteboard #5",
-        "type": "Whiteboard",
-        "status": "Available"
-    },
-    {
-        "id": "20",
-        "name": "Computer #5",
-        "type": "Computer",
-        "status": "Unavailable"
-    }
-	];
 	$.ajax({
-            url: '/inventory', 
-            type : "GET", 
+            url: 'http://localhost:8080/inventory', 
+            type : "GET",
+			headers: {
+				'X-Requested-With': readCookie("token"),
+				'Content-Type':'application/json'
+			},
+			dataType: "json",
             success : function(result) {
 				if (result.status) {
-					//make cookie
+					return result.resources;
 				}
                 // TODO: redirect to resources
                 console.log(result);
@@ -134,7 +17,7 @@ function getData() {
             error: function(xhr, resp, text) {
 				//need cookie
                 console.log(xhr, resp, text);
-                window.location.href = "Resources.html";
+                //window.location.href = "Resources.html";
                 // TODO: flash prompt for pass again
             }
         })
@@ -548,6 +431,31 @@ function getInventoryItem(id) {
 		}
 	}
 	return null;
+}
+
+function createCookie(name,value,days) {
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime()+(days*24*60*60*1000));
+        var expires = "; expires="+date.toGMTString();
+    }
+    else var expires = "";
+    document.cookie = name+"="+value+expires+"; path=/";
+}
+
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+function eraseCookie(name) {
+    createCookie(name,"",-1);
 }
 
 function getUrlParameter(sParam) {
